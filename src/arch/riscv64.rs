dbb93c16ed3b285c625e00a269ae6562fe027851
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2024-11-15 18:52:14 +0100

    Fixup for ddac7f5: Added usize arithmetics for riscv64::VirtAddr

--- src/arch/riscv64.rs
+++ src/arch/riscv64.rs
@@ -135,6 +135,41 @@ impl From<usize> for VirtAddr {
 }
 
 #[cfg(target_pointer_width = "64")]
+// if the target_pointer_width is 64, usize = u64 so we can safely add
+impl core::ops::Add<usize> for VirtAddr {
+    type Output = Self;
+    #[inline]
+    fn add(self, rhs: usize) -> Self::Output {
+        VirtAddr::new(self.0 + rhs as u64)
+    }
+}
+#[cfg(target_pointer_width = "64")]
+// if the target_pointer_width is 64, usize = u64 so we can safely add
+impl core::ops::AddAssign<usize> for VirtAddr {
+    #[inline]
+    fn add_assign(&mut self, rhs: usize) {
+        *self = *self + rhs;
+    }
+}
+#[cfg(target_pointer_width = "64")]
+// if the target_pointer_width is 64, usize = u64 so we can safely sub
+impl core::ops::Sub<usize> for VirtAddr {
+    type Output = Self;
+    #[inline]
+    fn sub(self, rhs: usize) -> Self::Output {
+        VirtAddr::new(self.0.checked_sub(rhs as u64).unwrap())
+    }
+}
+#[cfg(target_pointer_width = "64")]
+// if the target_pointer_width is 64, usize = u64 so we can safely sub
+impl core::ops::SubAssign<usize> for VirtAddr {
+    #[inline]
+    fn sub_assign(&mut self, rhs: usize) {
+        *self = *self - rhs;
+    }
+}
+
+#[cfg(target_pointer_width = "64")]
 // if the target_pointer_width is 64, usize = u64 so we can safely transform.
 impl From<usize> for PhysAddr {
     fn from(addr: usize) -> PhysAddr {
